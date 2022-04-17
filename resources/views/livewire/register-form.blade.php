d07712c88ab81cda4014354bceee8e7e8a3690cc
Mostafa Shaher <76525001+NeVeDlE@users.noreply.github.com>
2022-04-17 11:36:08 +0200

    Fixed some bugs

--- resources/views/livewire/register-form.blade.php
+++ resources/views/livewire/register-form.blade.php
@@ -10,28 +10,32 @@
                         <form action="{{ route('register') }}" method="POST" class="mt-2"
                               enctype="multipart/form-data">
                             @csrf
-                            <x-form.input name="name" wire="name"/>
-                            <x-form.input name="email" type="email" wire="email"/>
-                            <x-form.input name="birthday" type="date" wire="birthday"/>
+                            <x-form.input name="name" :value="old('name')" wire="name"/>
+                            <x-form.input name="email" :value="old('email')" type="email" wire="email"/>
+                            <x-form.input name="birthday" :value="old('birthday')" type="date" wire="birthday"/>
                             <x-form.input name="national_id" wire="national_id"/>
                             @if($show==true)
                                 <x-form.input name="password" wire="password" type="password"/>
-                                <button wire:click.prevent="toggle" >
+                                <button wire:click.prevent="toggle">
                                     <i class="far fa-eye" style="cursor: pointer;"></i>
                                 </button>
-                                <x-form.input name="password_confirmation" type="password" wire="password_confirmation"/>
+                                <x-form.input name="password_confirmation" type="password"
+                                              wire="password_confirmation"/>
                             @else
                                 <x-form.input name="password" wire="password"/>
-                                <button  wire:click.prevent="toggle">
+                                <button wire:click.prevent="toggle">
                                     <i class="far fa-eye-slash" style="cursor: pointer;"></i>
                                 </button>
-                                <x-form.input name="password_confirmation"  wire="password_confirmation"/>
+                                <x-form.input name="password_confirmation" wire="password_confirmation"/>
                             @endif
 
                             <x-form.input name="phone" wire="phone"/>
                             <x-form.input name="picture" type="file" wire="picture"/>
                             <div class="form-group mt-3 mb-5 btns1 btns">
-                                <x-submit-button>Submit</x-submit-button>
+                                @if(!$errors->any())
+                                    <x-submit-button >Submit</x-submit-button>
+
+                                @endif
                             </div>
                         </form>
                     </div>
