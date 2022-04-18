892ca1606639bf87bf90cf69b757ec598c842bd0
Mahmoud Ayman <m7m0udayman@gmail.com>
2022-04-18 03:17:47 -0700

    last updated files

--- resources/assets/scripts/main.js
+++ resources/assets/scripts/main.js
@@ -1,15 +1,12 @@
 // remember me function
-function setCookies(){
-    var date=new Date();
-    date.setMonth(date.getMonth(),12);
-    var usernamevalue=document.getElementById("username").value;
-    var passwordvalue=document.getElementById("password-filed").value;
-    document.cookie="remember-me-username="+usernamevalue+";expires="+date;
-    
+function setCookies() {
+  var date = new Date();
+  date.setMonth(date.getMonth(), 12);
+  var usernamevalue = document.getElementById("username").value;
+  var passwordvalue = document.getElementById("password-filed").value;
+  document.cookie =
+    "remember-me-username=" + usernamevalue + ";expires=" + date;
 }
-
-
-//slide show
 
 //Brevious and next forms and buttons
 const BackButton = document.querySelectorAll(".bckbtn"),
@@ -18,8 +15,9 @@ const BackButton = document.querySelectorAll(".bckbtn"),
 let FormStepsNumber = 0;
 
 NextButton.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    event.preventDefault();
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    // checkInputs();
     FormStepsNumber++;
     updateFormSteps();
     updateProgressBar();
@@ -51,48 +49,39 @@ function updateProgressBar() {
       ProgressStep.classList.remove("progress-step-active");
     }
   });
-//   const progressBarActive = document.querySelectorAll(".progress-step-active");
-//   ProgressBarLine.style.width =
-//     ((progressBarActive.length-1 ) / (ProgressSteps.length-1)) * 70 + "%";
 }
 
+let password = document.getElementById("password-filed"),
+  password1 = document.getElementById("password-filed1"),
+  password2 = document.getElementById("password-filed"),
+  toggle = document.getElementById("showpas"),
+  toggle1 = document.getElementById("showpas1"),
+  showIcon = document.getElementById("shopas"),
+  showIcon1 = document.getElementById("shopas1");
 
-let password=document.getElementById('password-filed'),
-    password1=document.getElementById('password-filed1'),
-    password2=document.getElementById('password-filed'),
-    toggle=document.getElementById('showpas'), 
-    toggle1=document.getElementById('showpas1'), 
-    showIcon=document.getElementById('shopas'),
-    showIcon1=document.getElementById('shopas1');
-
-function ShowHidePass(){
-    if(password.type==='password'){
-        password.type='text';
-        if(password1){
-        password1.type='text';
-        toggle1.style.color="#26A0DB";
-        }
-        toggle.style.color="#26A0DB";
-
+function ShowHidePass() {
+  if (password.type === "password") {
+    password.type = "text";
+    if (password1) {
+      password1.type = "text";
+      toggle1.style.color = "#26A0DB";
     }
-    else{
-        password.type='password';
-        if(password1){
-        password1.type='password';
-        toggle1.style.color="#0000004d";
-        }
-        toggle.style.color="#0000004d"; 
+    toggle.style.color = "#26A0DB";
+  } else {
+    password.type = "password";
+    if (password1) {
+      password1.type = "password";
+      toggle1.style.color = "#0000004d";
     }
-
+    toggle.style.color = "#0000004d";
+  }
 }
-function checkInput(){
-
+function checkInput() {}
+if (toggle || toogle1) {
+  toggle.addEventListener("click", ShowHidePass, false);
+  toggle1.addEventListener("click", ShowHidePass, false);
 }
-if(toggle||toogle1){
-toggle.addEventListener('click',ShowHidePass,false);
-toggle1.addEventListener('click',ShowHidePass,false);   
-}
-if(password||password1){
-password.addEventListener('keyup',checkInput,false);
-password1.addEventListener('keyup',checkInput,false);
+if (password || password1) {
+  password.addEventListener("keyup", checkInput, false);
+  password1.addEventListener("keyup", checkInput, false);
 }
